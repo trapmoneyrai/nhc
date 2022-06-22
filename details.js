9a941d88697410e530608b671175c38fe4c5c183
trapmoneyrai <97180669+trapmoneyrai@users.noreply.github.com>
2022-06-22 18:31:24 +0200

    done

---
+++ details.js
@@ -0,0 +1,5 @@
+const name = prompt("Enter your name:")
+const height = prompt("Enter your height in cm:")
+const country = prompt('Enter your country of residence:')
+
+document.write(name + "<br><br>" + height + "cm" + "<br><br>" + country)
